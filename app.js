1dc13954d84266caa89156b88fa4f73856d81ff6
dudi938 <dudi938@gmail.com>
2017-07-23 22:17:15 +0300

    fix

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+ //var index = require('./routes/index');
+ //var users = require('./routes/users');
 
 var app = express();
 
@@ -20,10 +20,17 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.use('/', index);
-app.use('/users', users);
+ //app.use('/', index);
+ //app.use('/users', users);
+
+
+app.get('/',function(req, res){
+  res.sendfile(path.join(__dirname,'public/index.html'))
+});
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,6 +49,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 
 module.exports = app;
