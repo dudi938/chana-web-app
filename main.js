a5831bad0dea9f2f709ff76c0dc02c5ddccede5c
dudi938 <dudi938@gmail.com>
2017-07-23 18:26:50 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+﻿var express = require('express');
+var path = require('path');
+var app = express();
+
+var mainPagePath  = path.join(__dirname,'\\public\\docs\\examples\\carousel\\index.html');
+//app.use(express.static(path.join(__dirname , 'public')));
+app.use("public", express.static(path.join(__dirname, 'public')));
+
+app.listen(3000,function(err){
+    if(err){
+        console.log(err);
+    }
+});
+
+
+app.get('/HomePage',function(req, res){
+    res.sendFile(mainPagePath,true);
+})
